d0e58df7de71fac8dd86253f2d8266875fd31cc4
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-01-24 23:31:40 -0800

    Add autocomplete

--- main/apps/canitake/static/js/main.js
+++ main/apps/canitake/static/js/main.js
@@ -1,14 +1,85 @@
 $(function() {
 	getAllDrugNames();
+
+	var autocompleteTarget;
+
+	$(".drug-input").focus(function() {
+		autocompleteTarget = $(this);
+		moveAutocompleteBoxTo(autocompleteTarget);
+
+		$("#autocomplete-container").empty();
+	});
+
+	$(window).resize(function() {
+		moveAutocompleteBoxTo(autocompleteTarget);
+	});
+
+	$("input").on("input", function() {
+		if (!window.AllDrugNames) return;
+
+		$("#autocomplete-container").empty();
+
+		var input = $(this).val().trim();
+
+		if (input) {
+			var suggestions = getAutocompleteSuggestions(input);
+
+			for (var i = 0; i < Math.min(suggestions.length, 5); i++) {
+				if (suggestions[i] == input) {
+					$("#autocomplete-container").empty();
+
+					break;
+				}
+
+				$("#autocomplete-container").append(`
+					<p class="autocomplete-suggestion noselect">${suggestions[i]}</p>
+				`)
+			}
+		}
+	});
+
+	$(document).on("click", ".autocomplete-suggestion", function() {
+		autocompleteTarget.val($(this).text());
+		$("#autocomplete-container").empty();
+	});
+
+	$("#first-drug").focus();
 });
 
 function getAllDrugNames() {
 	var url = "http://tripbot.tripsit.me/api/tripsit/getAllDrugAliases";
 	var proxy = "https://cors-anywhere.herokuapp.com/";
 
+	console.log("Getting all drug names...");
+
 	$.getJSON(proxy + url, function(response) {
 		if (!response.err) {
-			console.log(response.data);
+			console.log("Finished.")
+
+			window.AllDrugNames = response.data[0];
+		} else {
+			console.log("Error!")
 		}
 	});
+}
+
+function moveAutocompleteBoxTo($element) {
+	if ($element) {
+		$("#autocomplete-container").offset({
+			top: Math.round($element.offset().top + $element.parent().outerHeight() * 1.25),
+			left: Math.round($element.offset().left)
+		});
+	}
+}
+
+function getAutocompleteSuggestions(input) {
+	var suggestions = [];
+
+	window.AllDrugNames.forEach(function(drug) {
+		if (drug.startsWith(input)) {
+			suggestions.push(drug);
+		}
+	});
+
+	return suggestions;
 }
