ef20fa21e2d9248c3ce6aefd4a58d0d15e07b5bc
Dylan Bienenstock <dylanbienenstock@gmail.com>
2018-01-24 22:36:09 -0800

    Add tripsit.me API call

--- main/apps/canitake/static/js/main.js
+++ main/apps/canitake/static/js/main.js
@@ -1,3 +1,14 @@
 $(function() {
-	
+	getAllDrugNames();
 });
+
+function getAllDrugNames() {
+	var url = "http://tripbot.tripsit.me/api/tripsit/getAllDrugAliases";
+	var proxy = "https://cors-anywhere.herokuapp.com/";
+
+	$.getJSON(proxy + url, function(response) {
+		if (!response.err) {
+			console.log(response.data);
+		}
+	});
+}
